3e8b5f83802e86cedee7c2baf45af1409fb3aa32
goncalohgs <goncalohgs@gmail.com>
2024-09-29 22:48:48 +1000

    this creates function that prompt the user and returns a paragraph

--- .venv/BlogOpenAi.py
+++ .venv/BlogOpenAi.py
@@ -1,1 +1,15 @@
 # Generate a Blog with OpenAI 📝
+
+import openai
+
+def generate_blog(paragraph_topic):
+  response = openai.completions.create(
+    model = 'gpt-3.5-turbo-instruct',
+    prompt = 'Write a paragraph about the following topic. ' + paragraph_topic,
+    max_tokens = 400,
+    temperature = 0.3
+  )
+
+  retrieve_blog = response.choices[0].text
+
+  return retrieve_blog
