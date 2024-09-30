b3b2a7920df4c9ae8a43437cc41a200b5895d8a9
goncalohgs <goncalohgs@gmail.com>
2024-09-30 20:21:21 +1000

    Makes Intro

--- .venv/BlogOpenAi.py
+++ .venv/BlogOpenAi.py
@@ -7,6 +7,13 @@ config = dotenv_values('.env')
 
 openai.api_key = config['API_KEY']
 
+print("\n==========================Blog OpenAI=====================\n\n")
+print("==========================================================")
+print('"Intelligent Insights, Infinite Possibilities, Instantly"')
+print("==========================================================\n\n")
+
+
+
 def generate_blog(paragraph_topic):
   response = openai.completions.create(
     model = 'gpt-3.5-turbo-instruct',
@@ -20,9 +27,10 @@ def generate_blog(paragraph_topic):
 keep_writing = True
 
 while keep_writing:
-  answer = input('Write a paragraph? Y for yes, anything else for no. ')
+  answer = input('Hi I am BlogAI, at your service. Ask me anything! Y for yes, anything else for no. ')
   if (answer == 'Y'):
     paragraph_topic = input('What should this paragraph talk about? ')
     print(generate_blog(paragraph_topic))
   else:
     keep_writing = False
+
